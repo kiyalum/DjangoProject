8c5574ab805049e4bf5ac3dd8c5559488f2697a7
BurzakSerhii <burzak.serhii@gmail.com>
2026-09-23 13:01:56 +0200

    Add and register models(Student, Product)

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"Name: {self.name}; Age: {self.age}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Name: {self.name};  Price: {self.price};  Category: {self.category}"
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def test_views(request):
+    return HttpResponse("Hello World! 1")
+
+def test_views_2(request):
+    return render(request, "my_app/index.html", context={"info": "password"})
